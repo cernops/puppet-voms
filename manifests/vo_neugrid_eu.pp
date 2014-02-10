7acee20057a603f1230d6a2254de98dd66845a77
fschaer <frederic.schaer@cea.fr>
2014-02-10 09:51:10 +0100

    update neugrid voms

--- manifests/vo_neugrid_eu.pp
+++ manifests/vo_neugrid_eu.pp
@@ -2,9 +2,9 @@ class voms::vo_neugrid_eu {
   voms::client{'vo.neugrid.eu':
     servers => [
       {
-        server => 'voms.maatg.eu',
+        server => 'voms.gnubila.fr',
           port => '15001',
-            dn => '/O=GRID-FR/C=FR/O=CNRS/OU=CC-IN2P3/CN=cclcgvomsli01.in2p3.fr',
+            dn => '/O=GRID-FR/C=FR/O=MAATG/CN=voms.gnubila.fr',
          ca_dn => '/C=FR/O=CNRS/CN=GRID2-FR',
       },
     ]
