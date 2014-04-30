32afacf0be3c768cb30ee693eb2750252b212ffc
Alberto Peon <alberto.rodriguez.peon@cern.ch>
2014-04-30 16:38:58 +0200

    Add voms2 and lcg-voms2 servers for rest of the VOs

--- code/manifests/aleph.pp
+++ code/manifests/aleph.pp
@@ -25,6 +25,16 @@ class voms::aleph {
                     dn    => '/DC=ch/DC=cern/OU=computers/CN=lcg-voms.cern.ch',
                     ca_dn => '/DC=ch/DC=cern/CN=CERN Trusted Certification Authority'
                    },
+		   {server => 'voms2.cern.ch',
+		    port   => '15002',
+		    dn    => '/DC=ch/DC=cern/OU=computers/CN=voms2.cern.ch',
+		    ca_dn => '/DC=ch/DC=cern/CN=CERN Grid Certification Authority',
+		   },
+		   {server => 'lcg-voms2.cern.ch',
+		    port   => '15002',
+		    dn    => '/DC=ch/DC=cern/OU=computers/CN=lcg-voms2.cern.ch',
+		    ca_dn => '/DC=ch/DC=cern/CN=CERN Grid Certification Authority',
+		   }
                   ]
   }
 }
