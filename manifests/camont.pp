add373ed257b82583fb107d81f423a0b9de4ec53
kashif74 <kashif.alig@gmail.com>
2014-09-15 11:53:58 +0100

    fix gridpp voms files name

--- manifests/camont.pp
+++ manifests/camont.pp
@@ -9,13 +9,13 @@ voms::client { 'camont':
          ca_dn => '/C=UK/O=eScienceCA/OU=Authority/CN=UK e-Science CA 2B',
       },
       {
-        server => 'voms2.gridpp.ac.uk',
+        server => 'voms02.gridpp.ac.uk',
           port => '15025',
             dn => '/C=UK/O=eScience/OU=Oxford/L=OeSC/CN=voms02.gridpp.ac.uk',
          ca_dn => '/C=UK/O=eScienceCA/OU=Authority/CN=UK e-Science CA 2B',
       },
       {
-        server => 'voms3.gridpp.ac.uk',
+        server => 'voms03.gridpp.ac.uk',
           port => '15025',
             dn => '/C=UK/O=eScience/OU=Imperial/L=Physics/CN=voms03.gridpp.ac.uk',
          ca_dn => '/C=UK/O=eScienceCA/OU=Authority/CN=UK e-Science CA 2B',
--- manifests/vo_southgrid_ac_uk.pp
+++ manifests/vo_southgrid_ac_uk.pp
@@ -8,13 +8,13 @@ class voms::vo_southgrid_ac_uk {
          ca_dn => '/C=UK/O=eScienceCA/OU=Authority/CN=UK e-Science CA 2B',
       },
       {
-        server => 'voms2.gridpp.ac.uk',
+        server => 'voms02.gridpp.ac.uk',
           port => '15019',
             dn => '/C=UK/O=eScience/OU=Oxford/L=OeSC/CN=voms02.gridpp.ac.uk',
          ca_dn => '/C=UK/O=eScienceCA/OU=Authority/CN=UK e-Science CA 2B',
       },
       {
-        server => 'voms3.gridpp.ac.uk',
+        server => 'voms03.gridpp.ac.uk',
           port => '15019',
             dn => '/C=UK/O=eScience/OU=Imperial/L=Physics/CN=voms03.gridpp.ac.uk ',
          ca_dn => '/C=UK/O=eScienceCA/OU=Authority/CN=UK e-Science CA 2B',
