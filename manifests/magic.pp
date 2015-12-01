343ea0d945bb1101f25bbc129a6e86eeab657268
Chris Brew <chris.brew@stfc.ac.uk>
2015-12-01 16:08:40 +0000

    Add magic and lz vo configs

---
+++ manifests/magic.pp
@@ -0,0 +1,18 @@
+class voms::magic {
+  voms::client{'magic':
+    servers => [
+      {
+        server => 'voms01.pic.es',
+          port => '15003',
+            dn => '/DC=org/DC=terena/DC=tcs/C=ES/ST=Barcelona/L=Bellaterra/O=Port dInformacio Cientifica/CN=voms01.pic.es',
+         ca_dn => '/C=NL/ST=Noord-Holland/L=Amsterdam/O=TERENA/CN=TERENA eScience SSL CA 3',
+      },
+      {
+        server => 'voms02.pic.es',
+          port => '15003',
+            dn => '/DC=es/DC=irisgrid/O=pic/CN=voms02.pic.es',
+         ca_dn => '/DC=es/DC=irisgrid/CN=IRISGridCA',
+      },
+    ]
+  }
+}
