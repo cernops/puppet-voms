74171631196e73cb68f7a7298eee696518903821
Chris Brew <chris.brew@stfc.ac.uk>
2015-12-02 11:16:52 +0000

    Switch to new CERN voms servers for all CERN VOs

--- manifests/envirogrids.pp
+++ manifests/envirogrids.pp
@@ -16,15 +16,15 @@
 #unfortunately the full VO name envirogrids.vo.eu-egee.org is not a valid class name...
 class voms::envirogrids {
   voms::client{'envirogrids.vo.eu-egee.org':
-      servers  => [{server => 'voms.cern.ch',
+      servers  => [{server => 'voms2.cern.ch',
                     port   => '15002',
-                    dn    => '/DC=ch/DC=cern/OU=computers/CN=voms.cern.ch',
-                    ca_dn => '/DC=ch/DC=cern/CN=CERN Trusted Certification Authority'
+                    dn    => '/DC=ch/DC=cern/OU=computers/CN=voms2.cern.ch',
+                    ca_dn => '/DC=ch/DC=cern/CN=CERN Grid Certification Authority'
                    },
-                   {server => 'lcg-voms.cern.ch',
+                   {server => 'lcg-voms2.cern.ch',
                     port   => '15002',
-                    dn    => '/DC=ch/DC=cern/OU=computers/CN=lcg-voms.cern.ch',
-                    ca_dn => '/DC=ch/DC=cern/CN=CERN Trusted Certification Authority'
+                    dn    => '/DC=ch/DC=cern/OU=computers/CN=lcg-voms2.cern.ch',
+                    ca_dn => '/DC=ch/DC=cern/CN=CERN Grid Certification Authority'
                    }]
  }
 }
