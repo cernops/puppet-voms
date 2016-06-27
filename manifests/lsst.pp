d54a167447171a05a04d50b643a9beb16491217d
kashif74 <kashif.alig@gmail.com>
2016-06-27 17:31:15 +0100

    correction in lsst vo config

--- manifests/lsst.pp
+++ manifests/lsst.pp
@@ -10,13 +10,13 @@ class voms::lsst {
       {
         server => 'voms2.fnal.gov',
           port => '15003',
-            dn => '/DC=org/DC=opensciencegrid/O=Open Science Grid/OU=Services/CN=voms1.fnal.gov',
+            dn => '/DC=org/DC=opensciencegrid/O=Open Science Grid/OU=Services/CN=voms2.fnal.gov',
          ca_dn => '/DC=org/DC=cilogon/C=US/O=CILogon/CN=CILogon OSG CA 1',
       },
       {
         server => 'voms1.fnal.gov',
           port => '15003',
-            dn => '/DC=com/DC=DigiCert-Grid/O=Open Science Grid/OU=Services/CN=voms1.fnal.gov',
+            dn => '/DC=org/DC=opensciencegrid/O=Open Science Grid/OU=Services/CN=voms1.fnal.gov',
          ca_dn => '/DC=org/DC=cilogon/C=US/O=CILogon/CN=CILogon OSG CA 1',
       },
 
