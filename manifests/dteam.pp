95fcbdbe7f53b5f6a133628230e1d3d0c686c15f
kashif74 <kashif.alig@gmail.com>
2016-07-25 12:17:29 +0100

    Dteam VOMS configuration change

--- manifests/dteam.pp
+++ manifests/dteam.pp
@@ -1,14 +1,14 @@
 class voms::dteam {
   voms::client{'dteam': 
       servers => [{ server => 'voms.hellasgrid.gr',
-                    port   => '15001',
+                    port   => '15004',
                     dn    => '/C=GR/O=HellasGrid/OU=hellasgrid.gr/CN=voms.hellasgrid.gr',
-                    ca_dn => '/C=GR/O=HellasGrid/OU=Certification Authorities/CN=HellasGrid CA 2006'
+                    ca_dn => '/C=GR/O=HellasGrid/OU=Certification Authorities/CN=HellasGrid CA 2016'
                   },
                   { server => 'voms2.hellasgrid.gr',
-                    port   => '15001',
+                    port   => '15004',
                     dn    => '/C=GR/O=HellasGrid/OU=hellasgrid.gr/CN=voms2.hellasgrid.gr',
-                    ca_dn => '/C=GR/O=HellasGrid/OU=Certification Authorities/CN=HellasGrid CA 2006'
+                    ca_dn => '/C=GR/O=HellasGrid/OU=Certification Authorities/CN=HellasGrid CA 2016'
                  }]
   }
 }
