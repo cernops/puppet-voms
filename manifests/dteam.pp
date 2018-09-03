502fe64a6d584dd419d7b08e3ccc69fd8169b881
Petr Vokac <vokac@fjfi.cvut.cz>
2018-09-03 12:41:03 +0200

    Remove voms.hellasgrid.gr from dteam voms voms.hellasgrid.gr is unreachable and no longer mentioned in documentation https://operations-portal.egi.eu/vo/view/voname/dteam

--- manifests/dteam.pp
+++ manifests/dteam.pp
@@ -1,11 +1,6 @@
 class voms::dteam {
   voms::client{'dteam':
-      servers => [{ server                   => 'voms.hellasgrid.gr',
-                    port  => '15004',
-                    dn    => '/C=GR/O=HellasGrid/OU=hellasgrid.gr/CN=voms.hellasgrid.gr',
-                    ca_dn => '/C=GR/O=HellasGrid/OU=Certification Authorities/CN=HellasGrid CA 2016'
-                  },
-                  { server                   => 'voms2.hellasgrid.gr',
+      servers => [{ server                   => 'voms2.hellasgrid.gr',
                     port  => '15004',
                     dn    => '/C=GR/O=HellasGrid/OU=hellasgrid.gr/CN=voms2.hellasgrid.gr',
                     ca_dn => '/C=GR/O=HellasGrid/OU=Certification Authorities/CN=HellasGrid CA 2016'
